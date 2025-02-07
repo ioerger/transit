7b11fb4766bdb5712845e43ae3238985f68b41c7
ioerger2 <ioerger@cs.tamu.edu>
2025-02-07 10:12:37 -0600

    add pytransit.analysis to packages in setup.py; change package name back to 'tnseq-transit' for 1 final release on PyPi

--- setup.py
+++ setup.py
@@ -140,7 +140,8 @@ package_data = {
 }
 
 setup(
-    name='transit1',
+    name='tnseq-transit',
+    #name='transit1',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
@@ -181,7 +182,7 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     #packages = find_packages('src', exclude=['contrib', 'tests']), # any subdir in src/ with __init__.py
-    packages = ['pytransit.generic_tools', 'pytpp', 'pytransit'],
+    packages = ['pytransit.generic_tools', 'pytpp', 'pytransit', 'pytransit.analysis'],
     package_dir = {'pytransit': 'src/pytransit',  'pytpp': 'src/pytpp'},
     include_package_data=True,
     #py_modules = ['tpp'],
